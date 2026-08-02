dc27db3395fc9485a9a9e31e17dc4094fb058ea9
Ryuu10102005 <longnthh.2005@gmail.com>
2026-08-02 15:30:11 +0700

    Update project

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const {connectDB } = require('./db_connector');
 const { calculateScore } = require('./calculator_logic');
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 
 app.use(bodyParser.json());
 app.use(cors());
-
-let pool;
-(async () => {
-  pool = await connectDB();
-})();
 
 app.get('/api/journals', async (req, res) => {
   try {
@@ -50,27 +44,6 @@ app.post('/api/calculate', async (req, res) => {
     role_weight: parseFloat(role_weight),
     is_flagship: Boolean(is_flagship)
 });
-
-    await pool.query(
-      `INSERT INTO Papers 
-       (publisher, journal_name, sjr_percentile_p, cites_3_5y, role_weight, 
-        c95_tier_year, journal_base, journal_score, impact_score, paper_score, paper_score_adj)
-       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-      [
-        publisher,
-        journal_name,
-        sjr_percentile_p,
-        cites_3_5y,
-        role_weight,
-        result.C95,
-        result.JournalBase,
-        result.JournalScore,
-        result.ImpactScore,
-        result.PaperScore,
-        result.PaperScoreAdj
-      ]
-    );
-
     res.json({
       message: "✅ Tính toán thành công!",
       ...result
