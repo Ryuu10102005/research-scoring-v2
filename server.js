9c8506267494c93efd488945e6a9abad156c1f30
Ryuu10102005 <longnthh.2005@gmail.com>
2026-08-02 15:50:29 +0700

    Fix frontend path

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.post('/api/calculate', async (req, res) => {
 });
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 app.listen(port, () => {
   console.log(`✅ Backend Server đang chạy tại: http://localhost:${port}`);
